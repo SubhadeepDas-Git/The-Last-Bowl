d245ba62d4da40e85cdee580f733e4c35ac283a0
Subhadeep Das <studyac135@gmail.com>
2026-09-13 01:15:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/The-Last-Bowl/', // Ensures assets resolve properly on GitHub Pages sub-paths
   plugins: [
     tailwindcss(),
     react(),
   ],
 })
-
